67c67e038c98e946a30a35e6c598611e83e6d62f
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2021-02-25 20:28:10 -0800

    - Editin' files. - Lol documents... I don't wanna :cry:.. --- NOT YET!

--- AboutTheLugosEditor/Sources/EditorView.swift
+++ AboutTheLugosEditor/Sources/EditorView.swift
@@ -2,11 +2,30 @@ import SwiftUI
 import Combine
 
 struct EditorView: View {
+    
     @EnvironmentObject var editorState: ArticleEditorState
     
     var body: some View {
-        TextEditor(text: $editorState.articleBody)
-            .frame(minWidth: 300.0, minHeight: 320.0)
+        VStack {
+            HStack {
+                TextEditor(text: $editorState.articleBody)
+                    .frame(minWidth: 300.0, minHeight: 320.0)
+            }
+            HStack {
+                Button("Open Article") {
+                    openDirectory(receiveDirectory)
+                }
+            }
+        }
+    }
+    
+    func receiveDirectory(_ result: DirectoryResult) {
+        switch result {
+        case .success(let directory):
+            print(directory.childUrls)
+        case .failure(let error):
+            print(error)
+        }
     }
 }
 
---
+++ AboutTheLugosEditor/Sources/Models/FileUrlProvider.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+#if os(OSX)
+import AppKit
+#endif
+
+
+typealias FileResult = Result<URL, FileError>
+typealias FileReceiver = (FileResult) -> Void
+
+typealias DirectoryResult = Result<Directory, FileError>
+typealias DirectoryReceiver = (DirectoryResult) -> Void
+
+struct Directory {
+    let childUrls: [URL]
+}
+
+enum FileError: Error {
+    case generic
+}
+
+func openFile(_ receiver: @escaping FileReceiver) {
+    DispatchQueue.main.async {
+        let panel = NSOpenPanel()
+        panel.nameFieldLabel = "Choose a Swift source file to view"
+        panel.canChooseFiles = true
+        panel.canChooseDirectories = false
+        panel.canHide = true
+        panel.begin { response in
+            guard response == NSApplication.ModalResponse.OK,
+                let fileUrl = panel.url else {
+                receiver(.failure(.generic))
+                return
+            }
+            receiver(.success(fileUrl))
+        }
+    }
+}
+
+func openDirectory(_ receiver: @escaping DirectoryReceiver) {
+    DispatchQueue.main.async {
+        let panel = NSOpenPanel()
+        panel.nameFieldLabel = "Choose a directory to load all .swift files"
+        panel.canChooseFiles = false
+        panel.canChooseDirectories = true
+        panel.canHide = true
+        panel.begin { response in
+            guard response == NSApplication.ModalResponse.OK,
+                  let directoryUrl = panel.url else {
+                receiver(.failure(.generic))
+                return
+            }
+
+            guard let contents = try? FileManager.default.contentsOfDirectory(
+                at: directoryUrl,
+                includingPropertiesForKeys: nil,
+                options: .skipsSubdirectoryDescendants
+            ) else {
+                receiver(.failure(.generic))
+                return
+            }
+            
+            receiver(.success((Directory(childUrls: contents))))
+        }
+    }
+}
